f22e1f18e0a2b08f79b7bf96074a59fa41c5b724
dracero <diego.racero@gmail.com>
2022-12-07 00:04:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ async def geeter(name,classtype):
              "class": classtype}
 
 
-#if __name__ == "__main__":
-#   uvicorn.run(app, host="0.0.0.0", port="$PORT")
+if __name__ == "__main__":
+   uvicorn.run(app, host="0.0.0.0", port=process.env.PORT)
